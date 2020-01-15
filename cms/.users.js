dde5ad716b670abee786585a9efa849d334424e8
hinote <jehinote@gmail.com>
2020-01-15 21:09:04 +0000

    Testing

---
+++ cms/.users.js
@@ -0,0 +1,11 @@
+({
+	users: [
+		{
+			username: 'john',
+			tags: [],
+			salt: '458ebf9ef36262ee9ce4b1b2ba40635c',
+			hash: '7816425a371600b1f3ee83139fc538d92597b9c6d3f1c1883dcf5444c9ffa8f2',
+			user_created_timestamp: 1579122449912
+		}
+	]
+})
