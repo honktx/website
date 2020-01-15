b78c1cc11cb6ac3271a13e73d2e3de809858a606
hinote <jehinote@gmail.com>
2020-01-15 09:33:36 -0600

    Initial commit

---
+++ cms/global/global.js
@@ -0,0 +1,42 @@
+{
+	pages : [
+		{ 
+			label: '2020 Festival', url: '/',
+			submenu: [
+				{ label: 'Apply for 2020', url: '/2020-festival/apply', target: '_blank' },
+				{ label: 'Schedule', url: '/2020-festival/schedule' },
+				{ label: 'Bands', url: '/2020-festival/bands' },
+				{ label: 'FAQs', url: '/2020-festival/faq' },
+			]
+		},
+		{ label: 'Get Involved', url: '/get-involved', target: '_blank' },
+		{ label: 'Media', url: '/media' },
+		{ label: 'Supporters', url: '/supporters' },
+		{ label: 'History', url: '/history' }
+	],
+	donate : {
+		url : 'https://www.gofundme.com/honktx-9th-annual-festival'
+	},
+	social : [
+		{
+			name : 'Amazon',
+			icon_class : 'fab fa-amazon',
+			url : 'https://smile.amazon.com/ch/47-2409477'
+		},
+		{
+			name : 'Facebook',
+			icon_class : 'fab fa-facebook-f',
+			url : 'https://www.facebook.com/honktx'
+		},
+		{
+			name : 'Twitter',
+			icon_class : 'fab fa-twitter',
+			url : 'https://twitter.com/HonkTX'
+		},
+		{
+			name : 'Instagram',
+			icon_class : 'fab fa-instagram',
+			url : 'https://www.instagram.com/honktx/'
+		}
+	]
+}
---
+++ cms/index.js
@@ -0,0 +1,11 @@
+{
+	banner : {
+		src: '/assets/img/2020/HONKTX_2020_Promo_Graphic.jpg'
+	},
+	about : [
+		{
+			text: 'HONK!TX is a nonprofit, community-driven festival that brings street and brass bands from around the country to perform for free in the public spaces of Austin, Texas. Mobile and unamplified, HONK! bands transform everyday locations into spontaneous stages, dissolving the line between crowd and performers and inviting everyone to celebrate the joys of music and community. A range of genres will be represented: New Orleans second-line brass, European Klezmer and Balkan, Brazilian, West African and more. HONK! is a celebration of community, collaboration and creative expression. Join the revelry!',
+			color: 'white'
+		}
+	]
+}
